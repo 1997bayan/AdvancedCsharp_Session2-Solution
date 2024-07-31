505f604b0fa867ec9001d7948c56c1e15556e788
bayan.maged <bayan.maged@SW-BYANMAGED>
2024-07-31 14:35:51 +0300

    Add project files.

---
+++ AdvancedCsharp_Session2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvancedCsharp_Session2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
